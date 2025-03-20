0bf39beb3afe15807a9873d683fa47b36655a132
MgeeN0 <125311334+MgeeN0@users.noreply.github.com>
2025-03-20 16:39:03 +0200

    feat: Add export to PDF travel points function

--- Travel_Buddy/app/src/main/java/com/example/travel_buddy/MainActivity.kt
+++ Travel_Buddy/app/src/main/java/com/example/travel_buddy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.travel_buddy
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,8 +11,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.travel_buddy.classes_res.Travel_point
+import com.example.travel_buddy.classes_res.exportToPdf
 import com.example.travel_buddy.ui.theme.Travel_BuddyTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,6 +37,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     val test = Travel_point()
+    val test2 = Travel_point()
+    val test3 = Travel_point()
+    val travel_points = arrayOf(test,test2,test3)
+    val context = LocalContext.current
+    exportToPdf(context,"testpdf",travel_points)
     test.test()
     Text(
         text = "Hello $name!",
